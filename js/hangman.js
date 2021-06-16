835b614f32e8117551e4662c0d9feab1bee52ff0
sanjshres <sanjay.gopal.shrestha@gmail.com>
2021-06-16 22:35:53 +0545

    added all functions(rest, guessword, updateMistakes etc) -added all js functions

--- js/hangman.js
+++ js/hangman.js
@@ -4,19 +4,19 @@ var programming_languages = [
   "json",
   "java",
   "CSS",
-  "C",
-  "Csharp",
+  "c",
+  "csharp",
   "ruby",
   "sql",
-  "PHP",
-  "Kotlin",
+  "php",
+  "kotlin",
 ];
 
-let answer = "";
+let answer = '';
 let maxWrong = 6;
 let mistake = 0;
 let guessed = [];
-
+let wordStatus = null;
 function randomWord() {
   answer =
     programming_languages[
@@ -29,7 +29,7 @@ function generateButtons(){
     let buttonsHtml= "abcdefghijklmnopqrstuvwxyz".split('').map(letter =>
        ` <button
             class="btn btn-lg btn-primary m-2"
-            id='` + letter+` '
+            id='` + letter+ `'
             onClick="handleGuess('` + letter + `')"
         >
             ` + letter + `
@@ -37,8 +37,50 @@ function generateButtons(){
         `).join('');
         document.getElementById("keyboard").innerHTML = buttonsHtml;
 }
+function guessWord(){
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter: " _ " )).join('');
+    document.getElementById('wordSpotlight').innerHTML= wordStatus;
+}
 
-document.getElementById('maxWrong').innerHtml = maxWrong;
+document.getElementById('maxWrong').innerHTML = maxWrong;
 
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+    
+    if(answer.indexOf(chosenLetter) >= 0){
+        guessWord();
+        checkIfGameWon();
+    } else if(answer.indexOf(chosenLetter) === -1) {
+        mistake++;
+        updateMistakes();
+        checkIfGameLost();
+    }
+}
+function updateMistakes(){
+    document.getElementById('mistake').innerHTML = mistake;
+
+}
+function checkIfGameWon(){
+    if (wordStatus === answer){
+        document.getElementById('keyboard').innerHTML = "You Won!!!";
+    }
+}
+function checkIfGameLost(){
+    if (mistake === maxWrong){
+        document.getElementById('keyboard').innerHTML = "You Lost!!!";
+    }
+}
+function reset(){
+    mistake = 0;
+    guessed = [];
+    document.getElementById('hangmanPic').src = './images/0.jpg';
+    randomWord();
+    guessWord();
+    updateMistakes();
+    generateButtons();
+}
 randomWord();
 generateButtons();
+guessWord();
+handleGuess();
